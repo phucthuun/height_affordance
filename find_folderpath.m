73b984662ff78e1fd8e3d59f5847e173536615f3
phucthuun <nguyen@mpib-berlin.mpg.de>
2026-05-11 20:01:38 +0200

    Update find_folderpath.m

--- find_folderpath.m
+++ find_folderpath.m
@@ -4,16 +4,21 @@ function loc = find_folderpath()
     
 
 % Get the full path of the currently running script
-loc.script = mfilename('fullpath');
+loc.highorder = mfilename('fullpath');
 % Extract the folder path
-loc.root = fileparts(loc.script);
+loc.root = fileparts(loc.highorder);
 % Extract the project path
-loc.folder = string(regexp(loc.script, '.*?height_affordance', 'match'));
-loc.idgrap = string(regexp(loc.script, '.*?private', 'match'));
+loc.folder = string(regexp(loc.highorder, '.*?height_affordance', 'match'));
+loc.idgrap = string(regexp(loc.highorder, '.*?private', 'match'));
 
 % Get folder path to functions and configs, stimuli folder
+loc.script = fullfile(loc.root,'script');
 loc.function = fullfile(loc.root,'function');
 loc.stimuli = fullfile(loc.root,'stimuli','cam-165-select','cropped');
 % Specify path to retrieve and save data 
 loc.result = fullfile('C:\Users\exp-idgrap\Desktop\xplo-judo-data');
+
+addpath(loc.function);
+addpath(loc.stimuli);
+addpath(loc.result);
 end
