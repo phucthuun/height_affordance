7c8836d4bf296b4adddff349aaeb505b7b860131
phucthuun <nguyen@mpib-berlin.mpg.de>
2026-05-11 19:56:36 +0200

    Create Start_Task.m

---
+++ Start_Task.m
@@ -0,0 +1,11 @@
+%% 1. Preparations 
+sca;            % Close PTB windows
+close all;      % Close MATLAB figures
+clearvars;      % Clear variables
+clc;            % Clear command window
+totalTic = tic;
+
+%% Set paths
+loc = find_folderpath();
+taskLabel = task_info();
+run(fullfile(loc.script, sprintf("s01_Task_%s.m", taskLabel)));
