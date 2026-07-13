3e6c2050cac514a8907f00c0d771b31c8c4d7e91
phucthuun <nguyen@mpib-berlin.mpg.de>
2026-07-13 19:07:51 +0200

    Update subject_info2.m

--- script/function/subject_info2.m
+++ script/function/subject_info2.m
@@ -1,19 +1,22 @@
-function [subID, sesID, startRun, taskLabel] = subject_info2(loc, defaulttask)
+function [subID, sesID, startRun, taskLabel, PHONE_IP] = subject_info2(loc, defaulttask)
     name = 'BIDS Experimental Setup';
     
     prompt = {...
         'Subject ID (e.g., 001):', ...
         'Task ([t] training, [h] height-affordance, [e] estimate):', ...
         'Session ID (default S001):', ...
-        'Starting Run Number (default 001):'}; 
+        'Starting Run Number (default 001):', ...
+        'Neon Phone ([s] samsung, [m] motorola):'}; 
     
-    defaults = {'', defaulttask, 'S001', '001'};
+    defaults = {'', defaulttask, 'S001', '001', 's'};
     answer = inputdlg(prompt, name, 1, defaults); 
     
     if isempty(answer) || isempty(answer{1})
         errordlg('Subject ID is mandatory!'); 
         error('User cancelled or provided no ID.');
     end
+
+    subID = answer{1};
 
     taskInput = answer{2};
     switch taskInput
@@ -22,8 +25,6 @@ function [subID, sesID, startRun, taskLabel] = subject_info2(loc, defaulttask)
         case 'e',       taskLabel = 'estimate';
         otherwise,      taskLabel = lower(regexprep(taskInput, '\W', ''));
     end
-    
-    subID = answer{1};
     
     sesInput = answer{3};
     if startsWith(sesInput, 'S', 'IgnoreCase', true)
@@ -35,4 +36,10 @@ function [subID, sesID, startRun, taskLabel] = subject_info2(loc, defaulttask)
     
     startRun = str2double(answer{4}); % Convert to double so we can loop mathematically
     if isnan(startRun); startRun = 1; end
+
+    PHONE_Input = answer{5};
+    switch PHONE_Input
+        case 's',       PHONE_IP = '192.168.0.28';
+        case 'm',       PHONE_IP = '192.168.0.163';
+    end
 end
