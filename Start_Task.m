c88f97efdc0f627e4a622f969c1ed3745fe419ad
phucthuun <nguyen@mpib-berlin.mpg.de>
2026-07-23 20:33:10 +0200

    Update Start_Task.m
    reduce command line

--- Start_Task.m
+++ Start_Task.m
@@ -1,11 +1,2 @@
-%% 1. Preparations 
-sca;            % Close PTB windows
-close all;      % Close MATLAB figures
-clearvars;      % Clear variables
-clc;            % Clear command window
-totalTic = tic;
-
-%% Set paths
-loc = find_folderpath();
-taskLabel = task_info();
-run(fullfile(loc.script,sprintf("s01_Task_%s.m", taskLabel)));
+% THIS SCRIPT HAS ONE COMMAND LINE ONLY:
+run(fullfile('Start_Task_FULL.m'));
